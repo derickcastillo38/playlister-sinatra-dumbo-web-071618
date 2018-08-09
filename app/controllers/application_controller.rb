edf98ed19b72d3f5d328c3e0a2aa44b2714f007e
derickcastillo38 <9derick@gmail.com>
2018-08-09 17:49:32 -0400

    Passed the 4th test- only one left

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,42 @@ class ApplicationController < Sinatra::Base
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
-  get '/' do
-    erb :index
+  # get '/' do
+  #   erb :index
+  # end
+
+  get '/songs' do
+    @objects = Song.all
+    erb :list
+  end
+
+  get '/songs/new' do
+    erb :'song/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params["slug"])
+    erb :'song/show'
+  end
+
+  get '/artists' do
+    @objects = Artist.all
+    erb :list
+  end
+
+  get '/genres' do
+    @objects = Genre.all
+    erb :list
+  end
+
+  get '/artists/:slug' do
+    @artist = Artist.find_by_slug(params["slug"])
+    erb :'artist/show'
+  end
+
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params["slug"])
+    erb :'genre/show'
   end
 
 end
